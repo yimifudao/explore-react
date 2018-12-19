dcdc14940b43d50506589cf6a72693fe3dc6e27b
xuxuewen <xxw.code@aliyun.com>
2018-12-20 01:59:01 +0800

    fix lazy in routes

---
+++ src/pages/xxw/LazyError.js
@@ -0,0 +1,11 @@
+import React,{Fragment} from 'react';
+
+
+export default (props)=>{
+  console.log(props)
+  return (
+    <Fragment>
+      lazy
+    </Fragment>
+  )
+}
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React,{Suspense,lazy} from 'react';
 import { HashRouter as Router,Switch, Route, Redirect } from "react-router-dom";
 
 const XxwRoutes = lazy(()=>import('./pages/xxw.routes'))
+const LazyError = lazy(()=>import('./pages/xxw/LazyError'))
 class AppRoutes extends React.Component {
 
   render(){
@@ -9,9 +10,10 @@ class AppRoutes extends React.Component {
       <Router>
           <Switch>
             <Suspense fallback={<div>Loading...</div>}>
-              <Route path="/xxw" component={XxwRoutes} /> 
+              <Route path="/" component={props=><LazyError {...props} />} />
+              {/* <Route path="/xxw" component={XxwRoutes} />  */}
               {/* 默认路由 */}
-              <Redirect from="/" to="/xxw" />
+              {/* <Redirect from="/" to="/xxw" /> */}
             </Suspense>
           </Switch>
         </Router>
